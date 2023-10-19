deeb95d5f6888f85ee801e47398c2655d0dfd529
LJ <luisj.brasil@gmail.com>
2023-10-19 20:15:29 +0000

    alteração retorno

--- src/main/java/com/example/fn/HelloFunction.java
+++ src/main/java/com/example/fn/HelloFunction.java
@@ -3,10 +3,10 @@ package com.example.fn;
 public class HelloFunction {
 
     public String handleRequest(String input) {
-        String name = (input == null || input.isEmpty()) ? "world"  : input;
 
-        System.out.println("Inside Java Hello World function"); 
-        return "Hello, " + name + "!";
+
+        System.out.println("IFunction1 OK !!"); 
+        return "Function1 OK !!";
     }
 
 }
